ff80f1bf037d10efa72032b117b7a48dcfa20146
PythogrAL <alvinkihika3@gmail.com>
2022-06-30 12:25:20 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non zero otherwise
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+puts("\""Programming is like building a multilingual puzzle");
+return (0);
 }
