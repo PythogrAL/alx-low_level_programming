07aa9d6513b0158aa2ed6cf2316fdd00954d97b6
PythogrAL <alvinkihika3@gmail.com>
2022-07-01 12:47:10 -0700

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,29 +3,24 @@
 #include <time.h>
 
 /**
+ *  main - Entry point
  *
- * * main - Entry point
+ *   Return: returns 0 when everything works fine
  *
- * *
- *
- * * Return: returns 0 when everything works fine
- *
- * *
- *
- * */
+ */
 
 int main(void)
+{
+	int n;
 
-{	
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
-return (0);
 
-}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}	
