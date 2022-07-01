9caafdc7aa96a52ea3dfb1a41fdd38cf7abdce9f
PythogrAL <alvinkihika3@gmail.com>
2022-07-01 12:51:12 -0700

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  *  main - Entry point
  *
  *   Return: returns 0 when everything works fine
  *
  */
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
